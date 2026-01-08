81f045b8667bdf86947521736caca7dfd9d842a7
Dweb <24@ac.kr>
2026-01-08 21:58:19 +0900

    Add service, projects, contact sections

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+// pages.tsx
+
+import ServicesSection from "@/_component/servicesSection";
+import ProjectsSection from "@/_component/projects/projectsSection";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16">
+        <div className="flex flex-col items-center py-16 px-32 space-y-6 text-center">
+          <h1 className="text-5xl">Hi, I'm Sua Lee – Web Developer in South Korea</h1>
+          <h2 className="text-3xl mx-3">Computer Science Student & Web Developer</h2>
+          <p className="text-base mx-10 mb-10">I am a Computer Science student at Incheon National University,<br/>
+            building web applications with a focus on clarity, structure, and usability.<br></br>
+            I mainly work with Next.js and React and enjoy turning ideas into well-organized, 
+            functional interfaces.
+          </p>
+        </div>
+        <ServicesSection/>
+        <ProjectsSection/>
+        <div className="flex flex-col items-center justify-center my-14 p-2 space-y-3 border border-neutral-800 rounded-lg bg-neutral-900 w-6/11 h-40">
+          <p className="text-lg font-thin text-white text-center"> Ready to start your custom web development project? <br/> For a free consultation. </p>
+          <button           
+          type="button"
+          className="rounded-full bg-white px-8 py-2 text-base text-black"
+          >
+            Contact Sua
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
